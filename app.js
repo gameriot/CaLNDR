0405beff251bb1b70422585f57ffe11c99ffb577
Samast Varma <samastv99@gmail.com>
2017-09-23 01:43:44 -0400

    webhoook

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <"455717953">) {
+      req.query['hub.verify_token'] === <"EAAHMv9mv6T4BAGKNp0WnYzQZCgZCtXmLGzdyp65qHUkYwPTsEEdPXGFSZBmudy3IZBre0stelhhOZAp3GfqFcgoTgUzXTfoJZC7Xv4BT0lpJZCu6ylhhseiiZBSyRqsRALr960gZBSazyCUAWGU9QBHLNZBLS4y6FbRxue1L9e9xx98AZDZD">) {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
